6cb0fe916179624282a946f27ac6643a21779600
Francisco Garcia <jjjchico1@gmail.com>
2023-09-07 13:37:56 -0600

    feat: create users entity

--- src/modules/users/entities/user.entity.ts
+++ src/modules/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm'
+
+@Entity('users')
+export class User {
+  @PrimaryGeneratedColumn('increment', { type: 'int' })
+  id: number
+
+  @Column({ type: 'varchar', nullable: false })
+  username: string
+
+  @Column({ type: 'varchar', nullable: false })
+  password: string
+
+  @Column({ type: 'varchar', nullable: false })
+  email: string
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date
+}
