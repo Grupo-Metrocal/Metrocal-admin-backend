ef691ac22f42003f0a9d0df9b298a5256aae2d12
Francisco Garcia <jjjchico1@gmail.com>
2023-09-07 17:54:58 -0600

    feat: complete service to roles

--- src/modules/roles/roles.service.ts
+++ src/modules/roles/roles.service.ts
@@ -1,26 +1,38 @@
-import { Injectable } from '@nestjs/common';
-import { CreateRoleDto } from './dto/create-role.dto';
-import { UpdateRoleDto } from './dto/update-role.dto';
-
+import { Injectable, HttpException } from '@nestjs/common'
+import { CreateRoleDto } from './dto/create-role.dto'
+import { UpdateRoleDto } from './dto/update-role.dto'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { Role } from './entities/role.entity'
 @Injectable()
 export class RolesService {
-  create(createRoleDto: CreateRoleDto) {
-    return 'This action adds a new role';
+  constructor(
+    @InjectRepository(Role) private readonly roleRepository: Repository<Role>,
+  ) {}
+
+  async create(createRoleDto: CreateRoleDto): Promise<Role> {
+    try {
+      return await this.roleRepository.save(createRoleDto)
+    } catch (error) {
+      throw new HttpException(error.message, 400)
+    }
   }
 
-  findAll() {
-    return `This action returns all roles`;
+  async findAll(): Promise<Role[]> {
+    try {
+      const roles = await this.roleRepository.find()
+      return roles
+    } catch (error) {
+      throw new HttpException(error.message, 400)
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} role`;
-  }
-
-  update(id: number, updateRoleDto: UpdateRoleDto) {
-    return `This action updates a #${id} role`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} role`;
+  async findByName(name: string): Promise<Role> {
+    try {
+      const role = await this.roleRepository.find({ where: { name } })
+      return role[0]
+    } catch (error) {
+      throw new HttpException(error.message, 400)
+    }
   }
 }
