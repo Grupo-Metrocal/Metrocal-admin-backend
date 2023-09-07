2c991f9abc89f2bdb0a04bf9eca4d8c058fc6a20
Francisco Garcia <jjjchico1@gmail.com>
2023-09-07 17:56:36 -0600

    feat: user service repository

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,26 +1,58 @@
-import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { Injectable, HttpException } from '@nestjs/common'
+import { CreateUserDto } from './dto/create-user.dto'
+import { UpdateUserDto } from './dto/update-user.dto'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { User } from './entities/user.entity'
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
+  async create(createUserDto: CreateUserDto): Promise<User> {
+    try {
+      const user = await this.userRepository.save(createUserDto)
+      return user
+    } catch (error) {
+      throw new HttpException(error.message, 400)
+    }
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async deleteById(id: number): Promise<User> {
+    try {
+      const user = await this.findById(id)
+      await this.userRepository.remove(user)
+      return user
+    } catch (error) {
+      throw new HttpException(error.message, 400)
+    }
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findAll(): Promise<User[]> {
+    try {
+      const users = await this.userRepository.find()
+      return users
+    } catch (error) {
+      throw new HttpException(error.message, 400)
+    }
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async findById(id: number): Promise<User> {
+    try {
+      const user = await this.userRepository.find({ where: { id } })
+      return user[0]
+    } catch (error) {
+      throw new HttpException(error.message, 400)
+    }
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async findByEmail(email: string): Promise<User> {
+    try {
+      const user = await this.userRepository.find({ where: { email } })
+      return user[0]
+    } catch (error) {
+      throw new HttpException(error.message, 400)
+    }
   }
 }
