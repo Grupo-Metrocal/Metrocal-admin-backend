8c54570ebbd304fb132bda946db768f0780641f4
Francisco Garcia <jjjchico1@gmail.com>
2023-09-07 12:12:22 -0600

    feat: configure connection DB with TypeORM

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,30 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
       isGlobal: true,
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_POSTGRES_HOST,
+      database: process.env.DB_POSTGRES_DB,
+      port: parseInt(process.env.DB_POSTGRES_PORT),
+      password: process.env.DB_POSTGRES_PASSWORD,
+      username: process.env.DB_POSTGRES_USER,
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: false,
+      retryDelay: 3000,
+      retryAttempts: 10,
+      // ssl: {
+      //   /* <----- Add SSL option */
+      //   requestCert: true,
+      //   rejectUnauthorized: false,
+      // },
     }),
   ],
   controllers: [AppController],
