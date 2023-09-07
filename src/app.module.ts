8cb2d4bb7092b44301fd68679f82d90a093bb87f
Francisco Garcia <jjjchico1@gmail.com>
2023-09-07 13:33:20 -0600

    feat: generate resource users with CLI Nest

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { ConfigModule } from '@nestjs/config'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { UsersModule } from './modules/users/users.module';
 
 @Module({
   imports: [
@@ -27,6 +28,7 @@ import { TypeOrmModule } from '@nestjs/typeorm'
       //   rejectUnauthorized: false,
       // },
     }),
+    UsersModule,
   ],
   controllers: [AppController],
   providers: [AppService],
