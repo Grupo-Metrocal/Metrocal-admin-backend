060891ce3f64e10e5908f350a641590256dd1546
Francisco Garcia <jjjchico1@gmail.com>
2023-09-07 13:40:17 -0600

    feat: create roles entity

--- src/modules/roles/entities/role.entity.ts
+++ src/modules/roles/entities/role.entity.ts
@@ -1,1 +1,16 @@
-export class Role {}
+import { Column, Entity, PrimaryGeneratedColumn, ManyToOne } from 'typeorm'
+
+@Entity('roles')
+export class Role {
+  @PrimaryGeneratedColumn('increment', { type: 'int' })
+  id: number
+
+  @Column({ unique: true, nullable: false })
+  name: string
+
+  @Column({ nullable: false })
+  description: string
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  createdAt: Date
+}
