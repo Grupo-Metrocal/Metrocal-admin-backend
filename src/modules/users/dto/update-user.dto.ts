2b7f37a24799fa3b5f0f09e4b193adb29ea5abab
Francisco Garcia <jjjchico1@gmail.com>
2023-09-08 09:59:12 -0600

    fix: importation

--- src/modules/users/dto/update-user.dto.ts
+++ src/modules/users/dto/update-user.dto.ts
@@ -1,9 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types'
 import { IsNumber } from 'class-validator'
-import { userDto } from './user-dto'
+import { CreateUserDto } from './create-user.dto'
 import { ApiProperty } from '@nestjs/swagger'
 
-export class UpdateUserDto extends PartialType(userDto) {
+export class UpdateUserDto extends PartialType(CreateUserDto) {
   @ApiProperty()
   @IsNumber()
   id: number
