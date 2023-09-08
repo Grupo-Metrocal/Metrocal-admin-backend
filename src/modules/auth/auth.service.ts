6e431227209c1bbe3e019bd63c077cfb571bca17
Francisco Garcia <jjjchico1@gmail.com>
2023-09-08 15:34:50 -0600

    feat: user service in auth to logged user

--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,26 +1,30 @@
-import { Injectable } from '@nestjs/common';
-import { CreateAuthDto } from './dto/create-auth.dto';
-import { UpdateAuthDto } from './dto/update-auth.dto';
+import { Injectable, HttpException } from '@nestjs/common'
+import { SigninAuthDto } from './dto/signin-auth.dto'
+import { Repository } from 'typeorm'
+import { InjectRepository } from '@nestjs/typeorm'
+import { User } from '../users/entities/user.entity'
+import { compare } from 'bcrypt'
 
 @Injectable()
 export class AuthService {
-  create(createAuthDto: CreateAuthDto) {
-    return 'This action adds a new auth';
-  }
+  constructor(
+    @InjectRepository(User) private readonly userRepository: Repository<User>,
+  ) {}
 
-  findAll() {
-    return `This action returns all auth`;
-  }
+  async signin(user: SigninAuthDto) {
+    const { email, password } = user
 
-  findOne(id: number) {
-    return `This action returns a #${id} auth`;
-  }
+    const userFound = await this.userRepository.findOne({
+      where: { email },
+      select: ['id', 'username', 'password'],
+      // relations: ['roles'],
+    })
+    if (!userFound) throw new HttpException('Credenciales inválidas', 401)
 
-  update(id: number, updateAuthDto: UpdateAuthDto) {
-    return `This action updates a #${id} auth`;
-  }
+    const isMathPassword = await compare(password, userFound.password)
+    if (!isMathPassword) throw new HttpException('Credenciales inválidas', 401)
 
-  remove(id: number) {
-    return `This action removes a #${id} auth`;
+    delete userFound.password
+    return userFound
   }
 }
