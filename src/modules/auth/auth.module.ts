864a3463e07e05ca8a054a38bac27269cdcfbb54
Francisco Garcia <jjjchico1@gmail.com>
2023-09-08 15:33:57 -0600

    feat: import user module to auth provider

--- src/modules/auth/auth.module.ts
+++ src/modules/auth/auth.module.ts
@@ -1,9 +1,14 @@
-import { Module } from '@nestjs/common';
-import { AuthService } from './auth.service';
-import { AuthController } from './auth.controller';
+import { Module } from '@nestjs/common'
+import { AuthService } from './auth.service'
+import { AuthController } from './auth.controller'
+import { UsersModule } from '../users/users.module'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { User } from '../users/entities/user.entity'
+// import { Role } from '../roles/entities/role.entity'
 
 @Module({
+  imports: [UsersModule, TypeOrmModule.forFeature([User])],
   controllers: [AuthController],
-  providers: [AuthService],
+  providers: [AuthService, UsersModule],
 })
 export class AuthModule {}
