491e1167ff5d7d88e9a7e50ad768abce4d060218
Francisco Garcia <jjjchico1@gmail.com>
2023-09-13 15:50:59 -0600

    feat

--- src/modules/quotes/quotes.module.ts
+++ src/modules/quotes/quotes.module.ts
@@ -1,9 +1,23 @@
-import { Module } from '@nestjs/common';
-import { QuotesService } from './quotes.service';
-import { QuotesController } from './quotes.controller';
+import { Module } from '@nestjs/common'
+import { QuotesService } from './quotes.service'
+import { QuotesController } from './quotes.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Quote } from './entities/quote.entity'
+import { QuoteRequest } from './entities/quote-request.entity'
+import { EquipmentQuoteRequest } from './entities/equipment-quote-request.entity'
+import { User } from '../users/entities/user.entity'
 
 @Module({
+  imports: [
+    TypeOrmModule.forFeature([
+      Quote,
+      QuoteRequest,
+      EquipmentQuoteRequest,
+      User,
+    ]),
+  ],
   controllers: [QuotesController],
   providers: [QuotesService],
+  exports: [QuotesService],
 })
 export class QuotesModule {}
