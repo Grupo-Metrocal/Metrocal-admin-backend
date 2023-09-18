1e5da1d5f91c2d7ae6c60051c27d2f1e1295f121
Francisco Garcia <jjjchico1@gmail.com>
2023-09-18 17:49:03 -0600

    feat: import cliente Service and entity

--- src/modules/quotes/quotes.module.ts
+++ src/modules/quotes/quotes.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common'
 import { QuotesService } from './quotes.service'
 import { QuotesController } from './quotes.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
+import { ClientsService } from '../clients/clients.service'
 import { Quote } from './entities/quote.entity'
 import { QuoteRequest } from './entities/quote-request.entity'
 import { EquipmentQuoteRequest } from './entities/equipment-quote-request.entity'
 import { User } from '../users/entities/user.entity'
+import { Client } from '../clients/entities/client.entity'
 
 @Module({
   imports: [
@@ -14,10 +16,11 @@ import { User } from '../users/entities/user.entity'
       QuoteRequest,
       EquipmentQuoteRequest,
       User,
+      Client,
     ]),
   ],
   controllers: [QuotesController],
-  providers: [QuotesService],
+  providers: [QuotesService, ClientsService],
   exports: [QuotesService],
 })
 export class QuotesModule {}
