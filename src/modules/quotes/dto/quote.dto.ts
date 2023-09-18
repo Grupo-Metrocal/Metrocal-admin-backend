396b67bc1c05af77b5ede5eed1730a64d095eca9
Francisco Garcia <jjjchico1@gmail.com>
2023-09-18 17:53:11 -0600

    fix

--- src/modules/quotes/dto/quote.dto.ts
+++ src/modules/quotes/dto/quote.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger'
-import { IsNotEmpty, IsNumber } from 'class-validator'
+import { IsNotEmpty } from 'class-validator'
 import { QuoteRequest } from '../entities/quote-request.entity'
 import { User } from 'src/modules/users/entities/user.entity'
 
