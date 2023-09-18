615b03a29e359af16d17f57c89e4346d8c22c0c3
Francisco Garcia <jjjchico1@gmail.com>
2023-09-18 14:03:42 -0600

    feat: service, create client

--- src/modules/clients/clients.service.ts
+++ src/modules/clients/clients.service.ts
@@ -1,4 +1,17 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable } from '@nestjs/common'
+import { InjectRepository } from '@nestjs/typeorm'
+import { Repository } from 'typeorm'
+import { Client } from './entities/client.entity'
+import { CreateClientDto } from './dto/client.dto'
 
 @Injectable()
-export class ClientsService {}
+export class ClientsService {
+  constructor(
+    @InjectRepository(Client)
+    private readonly clientRepository: Repository<Client>,
+  ) {}
+
+  async createClient(client: CreateClientDto): Promise<CreateClientDto> {
+    return await this.clientRepository.save(client)
+  }
+}
