074b7412ffb779102090df1bc9e965e774bb49b0
Francisco Garcia <jjjchico1@gmail.com>
2023-09-18 14:02:36 -0600

    feat: controiller, create client

--- src/modules/clients/clients.controller.ts
+++ src/modules/clients/clients.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
-import { ClientsService } from './clients.service';
+import { Controller } from '@nestjs/common'
+import { ClientsService } from './clients.service'
+import { ApiTags } from '@nestjs/swagger'
+import { Post, Body } from '@nestjs/common'
+import { CreateClientDto } from './dto/client.dto'
 
+@ApiTags('clients')
 @Controller('clients')
 export class ClientsController {
   constructor(private readonly clientsService: ClientsService) {}
+
+  @Post()
+  async createClient(@Body() client: CreateClientDto) {
+    return await this.clientsService.createClient(client)
+  }
 }
