22d581ea1dec8b2b081a3b17c395c6b1cdcf54b4
Francisco Garcia <jjjchico1@gmail.com>
2023-09-18 12:22:26 -0600

    feat: quote controller

--- src/modules/quotes/quotes.controller.ts
+++ src/modules/quotes/quotes.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
-import { QuotesService } from './quotes.service';
+import { Controller } from '@nestjs/common'
+import { QuotesService } from './quotes.service'
+import { ApiTags } from '@nestjs/swagger'
+import { Post, Body, Get } from '@nestjs/common'
+import { QuoteRequestDto } from './dto/quote-request.dto'
 
+@ApiTags('quotes')
 @Controller('quotes')
 export class QuotesController {
   constructor(private readonly quotesService: QuotesService) {}
+
+  @Post('request')
+  async createQuoteRequest(@Body() quoteRequestDto: QuoteRequestDto) {
+    quoteRequestDto.status = 'pending'
+    return await this.quotesService.createQuoteRequest(quoteRequestDto)
+  }
+
+  @Get('request/all')
+  async getAllQuoteRequest() {
+    return await this.quotesService.getAllQuoteRequest()
+  }
+
+  @Post('request/reject')
+  async rejectQuoteRequest(@Body() id: number) {
+    return await this.quotesService.rejectQuoteRequest(id)
+  }
+
+  @Get('request/:id')
+  async getQuoteRequestById(@Body() id: number) {
+    return await this.quotesService.getQuoteRequestById(id)
+  }
 }
