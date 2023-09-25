e23ea0ff2ad6ce2d73b3f8d7537cc2ee1550a547
Francisco Garcia <jjjchico1@gmail.com>
2023-09-25 16:00:55 -0600

    feaat: import dependencies

--- src/modules/clients/clients.module.ts
+++ src/modules/clients/clients.module.ts
@@ -3,9 +3,19 @@ import { ClientsService } from './clients.service'
 import { ClientsController } from './clients.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Client } from './entities/client.entity'
+import { Quote } from '../quotes/entities/quote.entity'
+import { QuoteRequest } from '../quotes/entities/quote-request.entity'
+import { EquipmentQuoteRequest } from '../quotes/entities/equipment-quote-request.entity'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Client])],
+  imports: [
+    TypeOrmModule.forFeature([
+      Client,
+      Quote,
+      QuoteRequest,
+      EquipmentQuoteRequest,
+    ]),
+  ],
   controllers: [ClientsController],
   providers: [ClientsService],
   exports: [ClientsService],
