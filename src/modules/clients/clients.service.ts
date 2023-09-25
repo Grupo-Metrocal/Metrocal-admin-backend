3d6611129012869f7dd25cb6ae35205f1438393d
Francisco Garcia <jjjchico1@gmail.com>
2023-09-25 16:02:18 -0600

    feat: delete clien by ID & delete refereces foreign key

--- src/modules/clients/clients.service.ts
+++ src/modules/clients/clients.service.ts
@@ -3,12 +3,17 @@ import { InjectRepository } from '@nestjs/typeorm'
 import { Repository } from 'typeorm'
 import { Client } from './entities/client.entity'
 import { CreateClientDto } from './dto/client.dto'
+import { QuoteRequest } from '../quotes/entities/quote-request.entity'
+import { DataSource } from 'typeorm'
 
 @Injectable()
 export class ClientsService {
   constructor(
     @InjectRepository(Client)
     private readonly clientRepository: Repository<Client>,
+    @InjectRepository(QuoteRequest)
+    private readonly quoteRequestRepository: Repository<QuoteRequest>,
+    private readonly dataSource: DataSource,
   ) {}
 
   async createClient(client: CreateClientDto): Promise<CreateClientDto> {
@@ -23,5 +28,30 @@ export class ClientsService {
 
   async findAll(): Promise<Client[]> {
     return await this.clientRepository.find()
+  }
+
+  async delete(id: number): Promise<void> {
+    const client = await this.clientRepository.findOne({
+      where: { id },
+      relations: ['quote_requests'],
+    })
+
+    if (client.quote_requests.length > 0) {
+      const equipmentQuoteRequest = await this.quoteRequestRepository.find({
+        where: { client: { id } },
+        relations: ['equipment_quote_request', 'client'],
+      })
+
+      try {
+        await this.dataSource.transaction(async (manager) => {
+          await manager.remove(equipmentQuoteRequest)
+          await manager.remove(client)
+        })
+      } catch (error) {
+        throw new Error(error)
+      }
+    }
+
+    await this.clientRepository.delete({ id })
   }
 }
