f8c8ad61a8a9ef775f48f1f99620b7e6071e99e8
Francisco Garcia <jjjchico1@gmail.com>
2023-10-08 16:23:52 -0600

    feat: test pdf service

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,31 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Res } from '@nestjs/common'
 import { AppService } from './app.service'
+import { PdfService } from './modules/mail/pdf.service'
+import { Response } from 'express'
+import { ApiTags } from '@nestjs/swagger'
 
+@ApiTags('App')
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly pdfService: PdfService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello()
+  }
+
+  @Get('pdf')
+  async getPdf(@Res() res: Response) {
+    const pdf = await this.pdfService.generatePdf('template.hbs', {
+      name: 'John Doe',
+    })
+
+    // auto download
+    res.setHeader('Content-Type', 'application/pdf')
+    res.setHeader('Content-Disposition', 'attachment; filename=welcome.pdf')
+    res.send(pdf)
   }
 }
