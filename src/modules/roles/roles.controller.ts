efe84ea081861f50a0bcf9e6ee74039c29810d8e
Francisco Garcia <jjjchico1@gmail.com>
2023-10-19 17:23:00 -0600

    fix: untracket imports

--- src/modules/roles/roles.controller.ts
+++ src/modules/roles/roles.controller.ts
@@ -1,15 +1,6 @@
-import {
-  Controller,
-  Get,
-  Post,
-  Body,
-  Patch,
-  Param,
-  Delete,
-} from '@nestjs/common'
+import { Controller, Get, Post, Body, Param } from '@nestjs/common'
 import { RolesService } from './roles.service'
 import { CreateRoleDto } from './dto/create-role.dto'
-import { UpdateRoleDto } from './dto/update-role.dto'
 import { HttpException } from '@nestjs/common'
 import { ApiTags } from '@nestjs/swagger'
 import { handleBadrequest } from 'src/common/handleHttp'
