e9e484af28672afd184ddee767db028b8060e857
Francisco Garcia <jjjchico1@gmail.com>
2023-11-05 23:10:55 -0600

    feat: Add QuotesModule to ActivitiesModule imports.

--- src/modules/activities/activities.module.ts
+++ src/modules/activities/activities.module.ts
@@ -1,11 +1,15 @@
-import { Module } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { ActivitiesService } from './activities.service'
 import { ActivitiesController } from './activities.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Activity } from './entities/activities.entity'
+import { QuotesModule } from '../quotes/quotes.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Activity])],
+  imports: [
+    TypeOrmModule.forFeature([Activity]),
+    forwardRef(() => QuotesModule),
+  ],
   controllers: [ActivitiesController],
   providers: [ActivitiesService],
   exports: [ActivitiesService],
