811c01dec52ee68baa2d1a70281534b0ad0671fe
Francisco Garcia <jjjchico1@gmail.com>
2023-11-05 23:11:56 -0600

    feat: Add QuotesService and update createActivity method.

--- src/modules/activities/activities.service.ts
+++ src/modules/activities/activities.service.ts
@@ -2,12 +2,39 @@ import { Injectable } from '@nestjs/common'
 import { InjectRepository } from '@nestjs/typeorm'
 import { Activity } from './entities/activities.entity'
 import { Repository, DataSource } from 'typeorm'
+import { QuotesService } from '../quotes/quotes.service'
+import { handleInternalServerError, handleOK } from 'src/common/handleHttp'
 
 @Injectable()
 export class ActivitiesService {
-  constructor() // @InjectRepository(Activity)
-  // private readonly activitiesRepository: Repository<Activity>,
-  {}
+  constructor(
+    @InjectRepository(Activity)
+    private readonly activityRepository: Repository<Activity>,
+    private readonly quotesService: QuotesService,
+    private readonly dataSource: DataSource,
+  ) {}
 
-  async addActivity(activity: Activity) {}
+  async createActivity(activity: Activity) {
+    const quoteRequest = await this.quotesService.getQuoteRequestById(
+      activity.id,
+    )
+
+    const newActivity = this.activityRepository.create({
+      ...activity,
+      quote_request: quoteRequest,
+    })
+
+    quoteRequest.activity = newActivity
+
+    try {
+      await this.dataSource.transaction(async (manager) => {
+        await manager.save(newActivity)
+        await manager.save(quoteRequest)
+      })
+
+      return handleOK(newActivity)
+    } catch (error) {
+      return handleInternalServerError(error.message)
+    }
+  }
 }
