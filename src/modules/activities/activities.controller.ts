61bc32154ba3aeef79a71a12b7c5e8f460fca799
Francisco Garcia <jjjchico1@gmail.com>
2023-11-06 08:54:37 -0600

    feat: Add getAllActivities endpoint to ActivitiesController

--- src/modules/activities/activities.controller.ts
+++ src/modules/activities/activities.controller.ts
@@ -1,9 +1,15 @@
 import { Controller } from '@nestjs/common'
 import { ActivitiesService } from './activities.service'
 import { ApiTags } from '@nestjs/swagger'
+import { Get } from '@nestjs/common'
 
 @ApiTags('activities')
 @Controller('activities')
 export class ActivitiesController {
   constructor(private readonly activitiesService: ActivitiesService) {}
+
+  @Get()
+  async getAllActivities() {
+    return await this.activitiesService.getAllActivities()
+  }
 }
