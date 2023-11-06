39e2aa58ca6eb1635b62849bd7a92034fc7dbd03
Francisco Garcia <jjjchico1@gmail.com>
2023-11-05 23:09:52 -0600

    feat: Add ApiTags decorator to ActivitiesController.

--- src/modules/activities/activities.controller.ts
+++ src/modules/activities/activities.controller.ts
@@ -1,6 +1,8 @@
-import { Controller } from '@nestjs/common';
-import { ActivitiesService } from './activities.service';
+import { Controller } from '@nestjs/common'
+import { ActivitiesService } from './activities.service'
+import { ApiTags } from '@nestjs/swagger'
 
+@ApiTags('activities')
 @Controller('activities')
 export class ActivitiesController {
   constructor(private readonly activitiesService: ActivitiesService) {}
