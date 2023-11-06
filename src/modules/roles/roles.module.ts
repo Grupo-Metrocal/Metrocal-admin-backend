255637c6704377b334cf4d8526c00bf1147bee02
Francisco Garcia <jjjchico1@gmail.com>
2023-11-05 20:09:15 -0600

    feat: Add User entity and UsersModule to RolesModule imports

--- src/modules/roles/roles.module.ts
+++ src/modules/roles/roles.module.ts
@@ -1,12 +1,17 @@
-import { Module } from '@nestjs/common'
+import { Module, forwardRef } from '@nestjs/common'
 import { RolesService } from './roles.service'
 import { RolesController } from './roles.controller'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { Role } from './entities/role.entity'
 import { RolesInitializerService } from './roles.initializer.service'
+import { User } from '../users/entities/user.entity'
+import { UsersModule } from '../users/users.module'
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Role])],
+  imports: [
+    TypeOrmModule.forFeature([Role, User]),
+    forwardRef(() => UsersModule),
+  ],
   controllers: [RolesController],
   providers: [RolesService, RolesInitializerService],
   exports: [RolesService],
