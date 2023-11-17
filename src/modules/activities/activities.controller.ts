fce99959d59a8126f9f961d8b52e700e2134d2c5
Francisco Garcia <jjjchico1@gmail.com>
2023-11-17 13:34:26 -0600

    feat: add assignActivity endpoint to ActivitiesController

--- src/modules/activities/activities.controller.ts
+++ src/modules/activities/activities.controller.ts
@@ -1,7 +1,8 @@
 import { Controller } from '@nestjs/common'
 import { ActivitiesService } from './activities.service'
 import { ApiTags } from '@nestjs/swagger'
-import { Get } from '@nestjs/common'
+import { Get, Post, Body } from '@nestjs/common'
+import { AssignTeamMembersToActivityDto } from './dto/assign-activity.dt'
 
 @ApiTags('activities')
 @Controller('activities')
@@ -11,5 +12,12 @@ export class ActivitiesController {
   @Get()
   async getAllActivities() {
     return await this.activitiesService.getAllActivities()
+  }
+
+  @Post('assign-members')
+  async assignActivity(
+    @Body() assignActivityDto: AssignTeamMembersToActivityDto,
+  ) {
+    return await this.activitiesService.assignTeamMembers(assignActivityDto)
   }
 }
