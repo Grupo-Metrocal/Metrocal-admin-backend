af2f5332a57c319c3b5092c5ae4a9b3ae6ba9400
Francisco Garcia <jjjchico1@gmail.com>
2023-11-20 11:38:48 -0600

    fix: refact code

--- src/modules/configuration/configurations.module.ts
+++ src/modules/configuration/configurations.module.ts
@@ -1,26 +1,28 @@
-import { Injectable, Module, OnApplicationBootstrap, forwardRef } from '@nestjs/common'
+import {
+  Injectable,
+  Module,
+  OnApplicationBootstrap,
+  forwardRef,
+} from '@nestjs/common'
 import { TypeOrmModule } from '@nestjs/typeorm'
-import { Configuration, EquipmentRegister} from './entities/configuration.entity'
+import { Configuration } from './entities/configuration.entity'
+import { AuthorizedServices } from './entities/authorized_services.entity'
 import { ConfigurationService } from './configurations.service'
 import { ConfigurationController } from './configurations.controller'
 
 @Module({
-  imports: [
-    TypeOrmModule.forFeature([EquipmentRegister,Configuration])
-    
-  ],
+  imports: [TypeOrmModule.forFeature([AuthorizedServices, Configuration])],
   controllers: [ConfigurationController],
-  providers: [ConfigurationService, EquipmentRegister],
-  exports: [ConfigurationService, EquipmentRegister],
-  
+  providers: [ConfigurationService, AuthorizedServices],
+  exports: [ConfigurationService, AuthorizedServices],
 })
 export class ConfigurationModule {}
 
 @Injectable()
 export class ConfigInitializer implements OnApplicationBootstrap {
-  constructor(private readonly configService: ConfigurationService){}
-    
+  constructor(private readonly configService: ConfigurationService) {}
+
   onApplicationBootstrap() {
-     this.configService.createDefaultData()
+    this.configService.createDefaultData()
   }
 }
