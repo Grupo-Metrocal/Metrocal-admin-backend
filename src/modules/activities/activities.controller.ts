c9c5ef23fc4084a5aa54e7ac19417676cb0e7df7
Francisco Garcia <jjjchico1@gmail.com>
2023-12-01 09:58:26 -0600

    feat: Add/remove member functionality and authentication

--- src/modules/activities/activities.controller.ts
+++ src/modules/activities/activities.controller.ts
@@ -1,28 +1,45 @@
-import { Controller, Param } from '@nestjs/common'
+import { Controller, Delete, Param, UseGuards } from '@nestjs/common'
 import { ActivitiesService } from './activities.service'
 import { ApiTags } from '@nestjs/swagger'
 import { Get, Post, Body } from '@nestjs/common'
 import { AssignTeamMembersToActivityDto } from './dto/assign-activity.dt'
+import { RemoveMemberFromActivityDto } from './dto/remove-member.dto'
+import { JwtAuthGuard } from '../auth/guards/jwt-auth.guard'
+import { AddResponsableToActivityDto } from './dto/add-responsable.dto'
 
 @ApiTags('activities')
 @Controller('activities')
 export class ActivitiesController {
   constructor(private readonly activitiesService: ActivitiesService) {}
 
+  @UseGuards(JwtAuthGuard)
   @Get()
   async getAllActivities() {
     return await this.activitiesService.getAllActivities()
   }
 
+  @UseGuards(JwtAuthGuard)
   @Post('assign-members')
   async assignActivity(
     @Body() assignActivityDto: AssignTeamMembersToActivityDto,
   ) {
+    console.log(assignActivityDto)
     return await this.activitiesService.assignTeamMembers(assignActivityDto)
   }
 
+  @UseGuards(JwtAuthGuard)
   @Get('get-last-activities/:lastActivities')
   async getLastActivities(@Param('lastActivities') lastActivities: number) {
     return await this.activitiesService.getLastActivities(lastActivities)
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Delete('remove-member')
+  async removeMemberFromActivity(
+    @Body() removeMemberDto: RemoveMemberFromActivityDto,
+  ) {
+    return await this.activitiesService.removeMemberFromActivity(
+      removeMemberDto,
+    )
   }
 }
