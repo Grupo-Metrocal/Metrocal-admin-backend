6ed5d657951445ebfbd3799406f3f3c8838533ca
Francisco Garcia <jjjchico1@gmail.com>
2023-12-11 09:36:59 -0600

    feat: add Google OAuth2 for email authentication

--- src/modules/mail/mail.module.ts
+++ src/modules/mail/mail.module.ts
@@ -5,6 +5,7 @@ import { ConfigModule } from '@nestjs/config'
 import { join } from 'path'
 import { HandlebarsAdapter } from '@nestjs-modules/mailer/dist/adapters/handlebars.adapter'
 import { PdfService } from './pdf.service'
+import { google } from 'googleapis'
 
 @Module({
   imports: [
@@ -13,27 +14,44 @@ import { PdfService } from './pdf.service'
       isGlobal: true,
     }),
     MailerModule.forRootAsync({
-      useFactory: async () => ({
-        transport: {
-          host: process.env.MAILER_HOST,
-          port: +process.env.MAILER_PORT,
-          secure: true,
-          auth: {
-            user: process.env.MAILER_USER,
-            pass: process.env.MAILER_PASS,
+      useFactory: async () => {
+        const oauth2Client = new google.auth.OAuth2(
+          process.env.CLIENT_ID,
+          process.env.CLIENT_SECRET,
+          process.env.REDIRECT_URI,
+        )
+        oauth2Client.setCredentials({
+          refresh_token: process.env.REFRESH_TOKEN,
+        })
+
+        const accessToken = await oauth2Client.getAccessToken()
+
+        return {
+          transport: {
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+              type: 'OAuth2',
+              user: process.env.MAILER_USER,
+              clientId: process.env.CLIENT_ID,
+              clientSecret: process.env.CLIENT_SECRET,
+              refreshToken: process.env.REFRESH_TOKEN,
+              accessToken: accessToken.token,
+            },
           },
-        },
-        defaults: {
-          from: process.env.MAILER_FROM,
-        },
-        template: {
-          dir: join(__dirname, './templates'),
-          adapter: new HandlebarsAdapter(),
-          options: {
-            strict: true,
+          defaults: {
+            from: process.env.MAILER_FROM,
           },
-        },
-      }),
+          template: {
+            dir: join(__dirname, './templates'),
+            adapter: new HandlebarsAdapter(),
+            options: {
+              strict: true,
+            },
+          },
+        }
+      },
     }),
   ],
   controllers: [],
