e2ea490ee5d8deed30ededbc51148481d5c282ce
Francisco Garcia <jjjchico1@gmail.com>
2023-12-11 17:32:41 -0600

    fix: refactor QuotesService to use approvedQuoteRequestDto

--- src/modules/quotes/quotes.service.ts
+++ src/modules/quotes/quotes.service.ts
@@ -443,34 +443,34 @@ export class QuotesService {
 
     const token = this.tokenService.generateTemporaryLink(id, '15d')
 
-    const approvedQuoteRequestDto = new ApprovedQuoteRequestDto()
-
-    approvedQuoteRequestDto.clientName = quoteRequest.client.company_name
-    approvedQuoteRequestDto.servicesAndEquipments =
-      quoteRequest.equipment_quote_request.map((equipment) => {
-        return {
-          service: equipment.type_service,
-          equipment: equipment.name,
-          count: equipment.count,
-          unitPrice: equipment.price,
-          subTotal: equipment.total,
-          discount: equipment.discount,
-        }
-      })
-
-    approvedQuoteRequestDto.total = quoteRequest.price
-    approvedQuoteRequestDto.token = token
-    approvedQuoteRequestDto.email = quoteRequest.client.email
-    approvedQuoteRequestDto.linkDetailQuote = `${process.env.DOMAIN}/quote/${token}`
-    approvedQuoteRequestDto.subtotal =
-      quoteRequest.equipment_quote_request.reduce(
-        (acc, equipment) => acc + equipment.total,
-        0,
-      )
-    approvedQuoteRequestDto.tax = quoteRequest.tax
-    approvedQuoteRequestDto.discount = quoteRequest.general_discount
-
     try {
+      const approvedQuoteRequestDto = new ApprovedQuoteRequestDto()
+
+      approvedQuoteRequestDto.clientName = quoteRequest.client.company_name
+      approvedQuoteRequestDto.servicesAndEquipments =
+        quoteRequest.equipment_quote_request.map((equipment) => {
+          return {
+            service: equipment.type_service,
+            equipment: equipment.name,
+            count: equipment.count,
+            unitPrice: equipment.price,
+            subTotal: equipment.total,
+            discount: equipment.discount,
+          }
+        })
+
+      approvedQuoteRequestDto.total = quoteRequest.price
+      approvedQuoteRequestDto.token = token
+      approvedQuoteRequestDto.email = quoteRequest.client.email
+      approvedQuoteRequestDto.linkDetailQuote = `${process.env.DOMAIN}/quote/${token}`
+      approvedQuoteRequestDto.subtotal =
+        quoteRequest.equipment_quote_request.reduce(
+          (acc, equipment) => acc + equipment.total,
+          0,
+        )
+      approvedQuoteRequestDto.tax = quoteRequest.tax
+      approvedQuoteRequestDto.discount = quoteRequest.general_discount
+
       await this.mailService.sendMailApprovedQuoteRequest(
         approvedQuoteRequestDto,
       )
