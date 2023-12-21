dfdd30663f79275fa14712eec5100698a62ce77a
Francisco Garcia <jjjchico1@gmail.com>
2023-12-20 21:48:31 -0600

    feat: Add getAll and getAllNI_MCIT_P_01 methods to MethodsController

--- src/modules/methods/methods.controller.ts
+++ src/modules/methods/methods.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
-import { MethodsService } from './methods.service';
+import { Controller, Get } from '@nestjs/common'
+import { MethodsService } from './methods.service'
+import { ApiTags } from '@nestjs/swagger'
 
+@ApiTags('methods')
 @Controller('methods')
 export class MethodsController {
   constructor(private readonly methodsService: MethodsService) {}
+
+  @Get()
+  async getAll() {
+    return await this.methodsService.getAllMethods()
+  }
+
+  @Get('clear')
+  async getAllNI_MCIT_P_01() {
+    return await this.methodsService.deleteAllMethods()
+  }
 }
