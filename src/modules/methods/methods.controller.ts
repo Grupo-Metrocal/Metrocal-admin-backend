6c8806f861cf3876cc1befebc38f2c312e8bb80b
Francisco Garcia <jjjchico1@gmail.com>
2024-01-03 12:51:11 -0600

    feat: Add NI_MCIT_P_01 endpoints to MethodsController

--- src/modules/methods/methods.controller.ts
+++ src/modules/methods/methods.controller.ts
@@ -1,11 +1,16 @@
-import { Controller, Get } from '@nestjs/common'
+import { Controller, Get, Post, Body, Param } from '@nestjs/common'
 import { MethodsService } from './methods.service'
 import { ApiTags } from '@nestjs/swagger'
+import { NI_MCIT_P_01Service } from './ni-mcit-p-01.service'
+import { EquipmentInformationDto } from './dto/NI_MCIT_P_01/equipment_information.dto'
 
 @ApiTags('methods')
 @Controller('methods')
 export class MethodsController {
-  constructor(private readonly methodsService: MethodsService) {}
+  constructor(
+    private readonly methodsService: MethodsService,
+    private readonly ni_mcit_p_01Service: NI_MCIT_P_01Service,
+  ) {}
 
   @Get()
   async getAll() {
@@ -15,5 +20,21 @@ export class MethodsController {
   @Get('clear')
   async getAllNI_MCIT_P_01() {
     return await this.methodsService.deleteAllMethods()
+  }
+
+  @Post('ni-mcit-p-01/equipment-information/:methodId')
+  async createNI_MCIT_P_01EquipmentInformation(
+    @Body() equipment: EquipmentInformationDto,
+    @Param('methodId') methodId: number,
+  ) {
+    return await this.ni_mcit_p_01Service.equipmentInformation(
+      equipment,
+      methodId,
+    )
+  }
+
+  @Post('ni-mcit-p-01/create')
+  async createNI_MCIT_P_01() {
+    return await this.ni_mcit_p_01Service.create()
   }
 }
