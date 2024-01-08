0e8b78da9f6a081354719fcc3c09e376dd8638f0
Francisco Garcia <jjjchico1@gmail.com>
2024-01-08 10:35:10 -0600

    feat: Add endpoint for creating environmental conditions in NI_MCIT_P_01

--- src/modules/methods/methods.controller.ts
+++ src/modules/methods/methods.controller.ts
@@ -3,6 +3,7 @@ import { MethodsService } from './methods.service'
 import { ApiTags } from '@nestjs/swagger'
 import { NI_MCIT_P_01Service } from './ni-mcit-p-01.service'
 import { EquipmentInformationDto } from './dto/NI_MCIT_P_01/equipment_information.dto'
+import { EnvironmentalConditionsDto } from './dto/NI_MCIT_P_01/environmental_condition.dto'
 
 @ApiTags('methods')
 @Controller('methods')
@@ -29,6 +30,17 @@ export class MethodsController {
   ) {
     return await this.ni_mcit_p_01Service.equipmentInformation(
       equipment,
+      methodId,
+    )
+  }
+
+  @Post('ni-mcit-p-01/environmental-conditions/:methodId')
+  async createNI_MCIT_P_01EnvironmentalConditions(
+    @Body() environmentalConditions: EnvironmentalConditionsDto,
+    @Param('methodId') methodId: number,
+  ) {
+    return await this.ni_mcit_p_01Service.environmentalConditions(
+      environmentalConditions,
       methodId,
     )
   }
