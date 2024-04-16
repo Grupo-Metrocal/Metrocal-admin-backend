4eb6fdb315932d80d9c918a67297f6d58be2209c
Francisco Garcia <jjjchico1@gmail.com>
2024-04-16 14:58:40 -0600

    Add CreatePatternDto to handle pattern creation data transfer object

---
+++ src/modules/patterns/dto/create-patter.dto.ts
@@ -0,0 +1,40 @@
+import { ApiProperty } from '@nestjs/swagger'
+import { IsNotEmpty, IsString } from 'class-validator'
+import { IMethods } from 'src/modules/methods/entities/method.entity'
+
+export class CreatePatternDto {
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  method: IMethods
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  equipment: string
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  code: string
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  certificate: string
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  traceability: string
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  pattern_range?: string
+
+  @ApiProperty()
+  @IsNotEmpty()
+  @IsString()
+  next_calibration: string
+}
