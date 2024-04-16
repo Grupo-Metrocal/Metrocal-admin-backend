79eec55dd58fb3e4599136defca5bfd877d9dcca
Francisco Garcia <jjjchico1@gmail.com>
2024-04-16 14:58:32 -0600

    Add PatternsModule to handle pattern creation and retrieval

---
+++ src/modules/patterns/patterns.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common'
+import { PatternsService } from './patterns.service'
+import { PatternsController } from './patterns.controller'
+import { TypeOrmModule } from '@nestjs/typeorm'
+import { Pattern } from './entities/pattern.entity'
+import { PatternsInitializerService } from './patterns.initializer.service'
+
+@Module({
+  imports: [TypeOrmModule.forFeature([Pattern])],
+  controllers: [PatternsController],
+  providers: [PatternsService, PatternsInitializerService],
+})
+export class PatternsModule {}
