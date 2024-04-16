9bb9ce2939fbba16207ee6dad13f7f7c7c133dd5
Francisco Garcia <jjjchico1@gmail.com>
2024-04-16 14:58:44 -0600

    Add Pattern entity to handle pattern data in the database

---
+++ src/modules/patterns/entities/pattern.entity.ts
@@ -0,0 +1,32 @@
+import { IMethods } from 'src/modules/methods/entities/method.entity'
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm'
+
+@Entity('patterns')
+export class Pattern {
+  @PrimaryGeneratedColumn('increment', { type: 'int' })
+  id: number
+
+  @Column({ type: 'varchar', nullable: true })
+  method: IMethods
+
+  @Column({ type: 'varchar', nullable: false })
+  equipment: string
+
+  @Column({ type: 'varchar', nullable: false })
+  code: string
+
+  @Column({ type: 'varchar', nullable: false })
+  certificate: string
+
+  @Column({ type: 'varchar', nullable: false })
+  traceability: string
+
+  @Column({ type: 'varchar', nullable: false })
+  pattern_range?: string
+
+  @Column({ type: 'varchar', nullable: false })
+  next_calibration: string
+
+  @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+  created_at: Date
+}
