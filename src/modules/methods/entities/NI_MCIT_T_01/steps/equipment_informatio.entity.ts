b9ce5e49739f08f546487119673e24c26caa5716
Francisco Garcia <jjjchico1@gmail.com>
2024-04-22 15:17:52 -0600

    Add EquipmentInformationNI_MCIT_T_01 entity to NI_MCIT_T_01 module

---
+++ src/modules/methods/entities/NI_MCIT_T_01/steps/equipment_informatio.entity.ts
@@ -0,0 +1,48 @@
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm'
+import { NI_MCIT_T_01 } from '../NI_MCIT_T_01.entity'
+
+@Entity('equipment_information_NI_MCIT_T_01')
+export class EquipmentInformationNI_MCIT_T_01 {
+  @PrimaryGeneratedColumn()
+  id: number
+
+  @OneToMany(
+    () => NI_MCIT_T_01,
+    (NI_MCIT_P_01) => NI_MCIT_P_01.equipment_information,
+    {
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+  )
+  NI_MCIT_T_01: NI_MCIT_T_01[]
+
+  @Column({ type: 'varchar', nullable: true })
+  device?: string
+
+  @Column({ type: 'varchar', nullable: true })
+  maker?: string
+
+  @Column({ type: 'varchar', nullable: true })
+  serial_number?: string
+
+  @Column({ type: 'varchar', nullable: true })
+  measurement_range?: string
+
+  @Column({ type: 'float', nullable: true })
+  range_min?: number
+
+  @Column({ type: 'float', nullable: true })
+  range_max?: number
+
+  @Column({ type: 'varchar', nullable: true })
+  unit?: string
+
+  @Column({ type: 'varchar', nullable: true })
+  model?: string
+
+  @Column({ type: 'varchar', nullable: true })
+  code?: string
+
+  @Column({ type: 'float', nullable: true })
+  resolution?: number
+}
