c0f819bdfed084bdfe62d3fe9da3a8112e00ae1a
Francisco Garcia <jjjchico1@gmail.com>
2024-04-22 15:17:48 -0600

    Add EnvironmentalConditionsNI_MCIT_T_01 entity to NI_MCIT_T_01 module

---
+++ src/modules/methods/entities/NI_MCIT_T_01/steps/environmental_conditions.entity.ts
@@ -0,0 +1,43 @@
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm'
+import { NI_MCIT_T_01 } from '../NI_MCIT_T_01.entity'
+
+@Entity('environmental_conditions_NI_MCIT_T_01')
+export class EnvironmentalConditionsNI_MCIT_T_01 {
+  @PrimaryGeneratedColumn('increment')
+  id: number
+
+  @OneToMany(
+    () => NI_MCIT_T_01,
+    (NI_MCIT_P_01) => NI_MCIT_P_01.environmental_conditions,
+    {
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+  )
+  NI_MCIT_T_01: NI_MCIT_T_01[]
+
+  @Column('jsonb', { nullable: true })
+  cycles?: ICycles_NI_MCIT_T_01
+}
+
+export interface ICycles_NI_MCIT_T_01 {
+  cycle_number: number
+  ta: {
+    tac: {
+      initial: number
+      final: number
+    }
+    hrp: {
+      initial: number
+      final: number
+    }
+    equipement: string
+  }
+  hPa: {
+    pa: {
+      initial: number
+      final: number
+    }
+    equipement: string
+  }
+}
