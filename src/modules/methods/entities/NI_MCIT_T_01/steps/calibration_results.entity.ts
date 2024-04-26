0a8ecb5eb6e8e1ac0a2326b58fcc189afdd8e2f6
Francisco Garcia <jjjchico1@gmail.com>
2024-04-26 16:57:13 -0600

    Update calibration_results.entity.ts to include example data for results

--- src/modules/methods/entities/NI_MCIT_T_01/steps/calibration_results.entity.ts
+++ src/modules/methods/entities/NI_MCIT_T_01/steps/calibration_results.entity.ts
@@ -26,3 +26,169 @@ export interface IResults_NI_MCIT_T_01 {
     thermometer: number
   }[]
 }
+
+// example
+/*
+"results": [
+        {
+          "indication_linear": [
+            {
+              "patron": 0,
+              "thermometer": 0
+            },
+            {
+              "patron": 0,
+              "thermometer": 
+            },
+            {
+              "patron": 0,
+              "thermometer": 0
+            }, 
+          ]
+        },
+        {
+          "indication_linear": [
+            {
+              "patron": 10,
+              "thermometer": 10
+            },
+            {
+              "patron": 10,
+              "thermometer": 10
+            },
+            {
+              "patron": 10,
+              "thermometer": 10
+            },
+          ]
+        },
+        {
+          "indication_linear": [
+            {
+              "patron": 20,
+              "thermometer": 20
+            },
+            {
+              "patron": 20,
+              "thermometer": 20
+            },
+            {
+              "patron": 20,
+              "thermometer": 20
+            },
+          ]
+        },
+        {
+          "indication_linear": [
+            {
+              "patron": 30,
+              "thermometer": 30
+            },
+            {
+              "patron": 30,
+              "thermometer": 30
+            },
+            {
+              "patron": 30,
+              "thermometer": 30
+            },
+          ]
+        },
+        {
+          "indication_linear": [
+            {
+              "patron": 40,
+              "thermometer": 40
+            },
+            {
+              "patron": 40,
+              "thermometer": 40
+            },
+            {
+              "patron": 40,
+              "thermometer": 40
+            },
+          ]
+        },
+        {
+          "indication_linear": [
+            {
+              "patron": 50,
+              "thermometer": 50
+            },
+            {
+              "patron": 50,
+              "thermometer": 50
+            },
+            {
+              "patron": 50,
+              "thermometer": 50
+            },
+          ]
+        },
+        {
+          "indication_linear": [
+            {
+              "patron": 60,
+              "thermometer": 60
+            },
+            {
+              "patron": 60,
+              "thermometer": 60
+            },
+            {
+              "patron": 60,
+              "thermometer": 60
+            },
+          ]
+        },
+        {
+          "indication_linear": [
+            {
+              "patron": 70,
+              "thermometer": 70
+            },
+            {
+              "patron": 70,
+              "thermometer": 70
+            },
+            {
+              "patron": 70,
+              "thermometer": 70
+            },
+          ]
+        },
+        {
+          "indication_linear": [
+            {
+              "patron": 80,
+              "thermometer": 80
+            },
+            {
+              "patron": 80,
+              "thermometer": 80
+            },
+            {
+              "patron": 80,
+              "thermometer": 80
+            },
+          ]
+        },
+        {
+          "indication_linear": [
+            {
+              "patron": 90,
+              "thermometer": 90
+            },
+            {
+              "patron": 90,
+              "thermometer": 90
+            },
+            {
+              "patron": 90,
+              "thermometer": 90
+            },
+          ]
+        }
+      ]
+*/
