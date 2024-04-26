6ff78379dac280b1f0008ef5e85f98336d427ed3
Francisco Garcia <jjjchico1@gmail.com>
2024-04-26 16:57:05 -0600

    Fix certificate creation in NI_MCIT_P_01Service

--- src/modules/methods/ni-mcit-p-01.service.ts
+++ src/modules/methods/ni-mcit-p-01.service.ts
@@ -685,7 +685,7 @@ export class NI_MCIT_P_01Service {
         return handleOK('El método ya tiene un código de certificado')
       }
 
-      const certificate = await this.certificateService.create()
+      const certificate = await this.certificateService.create('P')
 
       method.certificate_code = certificate.data.code
       method.certificate_id = certificate.data.id
