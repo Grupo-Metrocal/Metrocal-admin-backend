b51e1d0e4b8e8544cf4106bbaca12d5d2295af5a
francisco-JGC <jjjchico1@gmail.com>
2024-06-03 16:00:42 -0600

    refactor: Remove unused firebase messaging code in NotificationsService

--- src/modules/notifications/notifications.service.ts
+++ src/modules/notifications/notifications.service.ts
@@ -1,31 +1,4 @@
 import { Injectable } from '@nestjs/common'
-import * as admin from 'firebase-admin'
 
 @Injectable()
-export class NotificationsService {
-  private readonly messaging: admin.messaging.Messaging
-
-  constructor() {
-    this.messaging = admin.messaging()
-  }
-
-  async sendNotification(
-    token: string,
-    title: string,
-    body: string,
-    data: any,
-  ) {
-    const message: admin.messaging.Message = {
-      token,
-      notification: {
-        title,
-        body,
-      },
-      data,
-    }
-
-    try {
-      await this.messaging.send(message)
-    } catch (error) {}
-  }
-}
+export class NotificationsService {}
