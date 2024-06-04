537e435e33fea32992765f292ffb990ef57d2817
francisco-JGC <jjjchico1@gmail.com>
2024-06-04 14:56:21 -0600

    feat: Add EquipmentInformationT05Dto for NI_MCIT_T_05 equipment information

---
+++ src/modules/methods/dto/NI_MCIT_T_05/equipment-information.dto.ts
@@ -0,0 +1,30 @@
+import { ApiProperty } from '@nestjs/swagger'
+
+export class EquipmentInformationT05Dto {
+  @ApiProperty()
+  device?: string
+
+  @ApiProperty()
+  temperature_min?: number
+
+  @ApiProperty()
+  temperature_max?: number
+
+  @ApiProperty()
+  maker?: string
+
+  @ApiProperty()
+  unit?: string
+
+  @ApiProperty()
+  model?: string
+
+  @ApiProperty()
+  serial_number?: string
+
+  @ApiProperty()
+  code?: string
+
+  @ApiProperty()
+  resolution?: number
+}
