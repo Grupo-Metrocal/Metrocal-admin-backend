9d724b3506d2fa4c3a6e45095a6e653cd4399d80
francisco-JGC <jjjchico1@gmail.com>
2024-06-12 15:16:15 -0600

    feat: Add EnvironmentalConditionsNI_MCIT_V_01 entity

---
+++ src/modules/methods/entities/NI_MCIT_V_01/steps/environmental_conditions.entity.ts
@@ -0,0 +1,34 @@
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm'
+import { NI_MCIT_V_01 } from '../NI_MCIT_V_01.entity'
+
+@Entity('environmental_conditions_NI_MCIT_V_01')
+export class EnvironmentalConditionsNI_MCIT_V_01 {
+  @PrimaryGeneratedColumn('increment')
+  id: number
+
+  @OneToMany(
+    () => NI_MCIT_V_01,
+    (NI_MCIT_V_01) => NI_MCIT_V_01.environmental_conditions,
+    {
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+  )
+  NI_MCIT_V_01: NI_MCIT_V_01[]
+
+  @Column('jsonb', { nullable: true })
+  points?: IPoints_NI_MCIT_V_01[]
+}
+
+export interface IPoints_NI_MCIT_V_01 {
+  point_number: number
+  temperature: IConditions
+  humidity: IConditions
+  presion_pa: IConditions
+}
+
+interface IConditions {
+  initial: number
+  final: number
+  resolution: number
+}
