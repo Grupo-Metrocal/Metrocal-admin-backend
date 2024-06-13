1cd552db9ccfc7e755b389c7b32ee23ac116d30f
francisco-JGC <jjjchico1@gmail.com>
2024-06-13 13:48:11 -0600

    refactor: Update ApiProperty in EnvironmentalConditionsV01Dto to include type information

--- src/modules/methods/dto/NI_MCIT_V_01/environmental_condition.dto.ts
+++ src/modules/methods/dto/NI_MCIT_V_01/environmental_condition.dto.ts
@@ -2,6 +2,6 @@ import { ApiProperty } from '@nestjs/swagger'
 import { IPoints_NI_MCIT_V_01 } from '../../entities/NI_MCIT_V_01/steps/environmental_conditions.entity'
 
 export class EnvironmentalConditionsV01Dto {
-  @ApiProperty()
+  @ApiProperty({ type: [Object] })
   points?: IPoints_NI_MCIT_V_01[]
 }
