0111fe522ec93853191245ad9402535755d1c069
francisco-JGC <jjjchico1@gmail.com>
2024-06-19 12:48:28 -0600

    feat: Update equipment_information.dto.ts with code and maker properties

--- src/modules/methods/dto/NI_MCIT_M_01/equipment_information.dto.ts
+++ src/modules/methods/dto/NI_MCIT_M_01/equipment_information.dto.ts
@@ -2,29 +2,15 @@ import { ApiProperty } from '@nestjs/swagger'
 
 export class EquipmentInformationDto {
   @ApiProperty()
-  certificate_id?: string
-
-  @ApiProperty()
-  applicant?: string
+  code?: string
 
   @ApiProperty()
   calibration_object?: string
 
   @ApiProperty()
-  manofacturer_brand?: string
+  maker?: string
 
   @ApiProperty()
   model?: string
 
-  @ApiProperty()
-  address_applicant?: string
-
-  @ApiProperty()
-  calibration_place?: string
-
-  @ApiProperty()
-  calibration_date?: string
-
-  @ApiProperty()
-  Service_code?: string
 }
