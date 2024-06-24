e6db8513095556ba28d7a6d58ebb428d20090fc0
francisco-JGC <jjjchico1@gmail.com>
2024-06-24 12:02:46 -0600

    fix: Update equipment information values to be numbers

--- src/modules/methods/ni-mcit-v-01.service.ts
+++ src/modules/methods/ni-mcit-v-01.service.ts
@@ -381,11 +381,11 @@ export class NI_MCIT_V_01Service {
           `${method.equipment_information.scale_division} ${method.equipment_information.unit}`,
         )
 
-      sheet.cell('K5').value(equipment_information.material)
-      sheet.cell('N5').value(equipment_information.balance)
-      sheet.cell('P5').value(equipment_information.neck_diameter)
-      sheet.cell('R5').value(equipment_information.thermometer)
-      sheet.cell('U4').value(equipment_information.volumetric_container)
+      sheet.cell('K5').value(Number(equipment_information.material))
+      sheet.cell('N5').value(Number(equipment_information.balance))
+      sheet.cell('P5').value(Number(equipment_information.neck_diameter))
+      sheet.cell('R5').value(Number(equipment_information.thermometer))
+      sheet.cell('U4').value(Number(equipment_information.volumetric_container))
 
       // Environmental Conditions
 
