66f51041ee78ef65784d377ad0390d25070feb42
francisco-JGC <jjjchico1@gmail.com>
2024-06-24 14:31:10 -0600

    refactor: Update formatDate function to use yyyy/mm/dd format

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,8 +1,5 @@
 export const formatDate = (date: string) => {
   const dateObj = new Date(date);
 
-  // Format date as año/mes/día
-  // 01/01/2021
-
   return `${dateObj.getFullYear()}/${dateObj.getMonth() + 1}/${dateObj.getDate()}`;
 }
