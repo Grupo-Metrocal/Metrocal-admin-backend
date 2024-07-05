77860cf0e3eedf3af29775f999f57bb25d4ef399
francisco-JGC <jjjchico1@gmail.com>
2024-07-05 17:13:25 -0600

    refactor: Update formatPrices.ts to use consistent formatting

--- src/utils/formatPrices.ts
+++ src/utils/formatPrices.ts
@@ -1,6 +1,6 @@
 export const formatPrice = (price = 0) => {
   return new Intl.NumberFormat('fr-FR', {
     minimumFractionDigits: 2,
-    maximumFractionDigits: 2
-  }).format(price);
+    maximumFractionDigits: 2,
+  }).format(price)
 }
