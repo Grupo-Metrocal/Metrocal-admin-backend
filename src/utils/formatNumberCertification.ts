e8d488f21852a4f1e8ee6961286fb722e7340ac9
francisco-JGC <jjjchico1@gmail.com>
2024-07-24 15:40:31 -0600

    refactor: Update formatNumberCertification to handle string input

--- src/utils/formatNumberCertification.ts
+++ src/utils/formatNumberCertification.ts
@@ -1,6 +1,38 @@
-export const formatNumberCertification = (number = 0, fraction = 1) => {
+export const formatNumberCertification = (
+  number: string | number,
+  fraction = 1,
+) => {
+  const convertNumber = typeof number === 'string' ? Number(number) : number
+
+  if (isNaN(convertNumber)) {
+    return number
+  }
+
+  if (fraction === 0) {
+    return number
+  }
+
   return new Intl.NumberFormat('fr-FR', {
     minimumFractionDigits: fraction,
     maximumFractionDigits: fraction,
-  }).format(number)
+  }).format(convertNumber)
+}
+
+export const formatSameNumberCertification = (number = 0) => {
+  const fractions = number.toString().split('.')
+  return formatNumberCertification(
+    number,
+    fractions[1] ? fractions[1].length : 0,
+  )
+}
+
+export const convertToValidNumber = (value: string) => {
+  let cleanedStr = value.replace(/,/g, '')
+  const number = parseFloat(cleanedStr)
+
+  if (isNaN(number)) {
+    return value
+  }
+
+  return number
 }
