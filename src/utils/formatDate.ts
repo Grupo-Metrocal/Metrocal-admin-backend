6dfe9385433d3e2ea9b7324a969dfea1b54b946e
francisco-JGC <jjjchico1@gmail.com>
2024-07-25 13:47:22 -0600

    refactor: Update formatDate to include leading zero for month in formatted date

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,5 +1,9 @@
 export const formatDate = (date: string) => {
-  const dateObj = new Date(date);
+  const dateObj = new Date(date)
+  const month =
+    dateObj.getMonth() + 1 < 10
+      ? `0${dateObj.getMonth() + 1}`
+      : dateObj.getMonth() + 1
 
-  return `${dateObj.getFullYear()}-${dateObj.getMonth() + 1}-${dateObj.getDate()}`;
+  return `${dateObj.getFullYear()}-${month}-${dateObj.getDate()}`
 }
