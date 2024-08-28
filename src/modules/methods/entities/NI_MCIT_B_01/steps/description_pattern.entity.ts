3d2c82affdcb5f6d6a42b575a48beec1ec90a0d4
francisco-JGC <jjjchico1@gmail.com>
2024-08-28 09:46:45 -0600

    feat: Add DescriptionPatternNI_MCIT_B_01 entity and update NI_MCIT_B_01.entity.ts

---
+++ src/modules/methods/entities/NI_MCIT_B_01/steps/description_pattern.entity.ts
@@ -0,0 +1,24 @@
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm'
+import { NI_MCIT_B_01 } from '../NI_MCIT_B_01.entity'
+
+@Entity('description_pattern_NI_MCIT_B_01')
+export class DescriptionPatternNI_MCIT_B_01 {
+  @PrimaryGeneratedColumn('increment')
+  id: number
+
+  @OneToMany(
+    () => NI_MCIT_B_01,
+    (NI_MCIT_B_01) => NI_MCIT_B_01.description_pattern,
+    {
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE',
+    },
+  )
+  NI_MCIT_B_01: NI_MCIT_B_01[]
+
+  @Column({ nullable: true })
+  observation?: string
+
+  @Column({ nullable: true })
+  creditable?: boolean
+}
