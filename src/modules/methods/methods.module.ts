107fff7d44d1f3bc01260ece479c1cb0924575c2
francisco-JGC <jjjchico1@gmail.com>
2024-08-28 09:46:22 -0600

    refactor: Add DescriptionPatternNI_MCIT_B_01 entity to MethodsModule

--- src/modules/methods/methods.module.ts
+++ src/modules/methods/methods.module.ts
@@ -59,6 +59,7 @@ import { EquipmentInformationNI_MCIT_B_01 } from './entities/NI_MCIT_B_01/steps/
 import { EccentricityTestNI_MCIT_B_01 } from './entities/NI_MCIT_B_01/steps/b01eccentricity_test.entity'
 import { RepeatabilityTestNI_MCIT_B_01 } from './entities/NI_MCIT_B_01/steps/b01repeatability_test.entity'
 import { LinearityTestNI_MCIT_B_01 } from './entities/NI_MCIT_B_01/steps/b01linearity_test.entity'
+import { DescriptionPatternNI_MCIT_B_01 } from './entities/NI_MCIT_B_01/steps/description_pattern.entity'
 
 import { NI_MCIT_T_03Service } from './ni-mcit-t-03.service'
 import { NI_MCIT_T_03 } from './entities/NI_MCIT_T_03/NI_MCIT_T_03.entity'
@@ -134,6 +135,7 @@ import { GENERIC_METHODService } from './generic-method.service'
       EccentricityTestNI_MCIT_B_01,
       RepeatabilityTestNI_MCIT_B_01,
       LinearityTestNI_MCIT_B_01,
+      DescriptionPatternNI_MCIT_B_01,
 
       NI_MCIT_T_03,
       EquipmentInformationNI_MCIT_T_03,
