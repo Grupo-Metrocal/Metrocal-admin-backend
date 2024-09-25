b1787768341fc9aeafcfc84bc6d8fc9cde068035
francisco-JGC <jjjchico1@gmail.com>
2024-09-25 10:33:29 -0600

    fix: format price with NIO

--- src/utils/formatPrices.ts
+++ src/utils/formatPrices.ts
@@ -1,6 +1,12 @@
-export const formatPrice = (price = 0) => {
+export const formatPrice = (price = 0, currency = 0, currencyType = 'USD') => {
+  if (currencyType === 'USD')
+    return new Intl.NumberFormat('fr-FR', {
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    }).format(price)
+
   return new Intl.NumberFormat('fr-FR', {
     minimumFractionDigits: 2,
     maximumFractionDigits: 2,
-  }).format(price)
+  }).format(price * currency)
 }
