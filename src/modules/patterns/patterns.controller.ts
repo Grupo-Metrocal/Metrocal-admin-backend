ccdc9c253bd2f22085f2cb69a5b600e83d500c3c
MJackson22-bit <jm5444859@gmail.com>
2024-10-15 17:06:50 -0600

    feat: add new service in the pattern module

--- src/modules/patterns/patterns.controller.ts
+++ src/modules/patterns/patterns.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body, Param, Get } from '@nestjs/common'
+import { Controller, Post, Body, Param, Get, Put, Delete } from '@nestjs/common'
 import { PatternsService } from './patterns.service'
 import { ApiTags } from '@nestjs/swagger'
 import { UseGuards } from '@nestjs/common'
@@ -14,8 +14,34 @@ export class PatternsController {
     return await this.patternsService.createPattern(pattern)
   }
 
+  @Put(':pattern_id')
+  async remove(@Param('pattern_id') pattern_id: number) {
+    return await this.patternsService.remove(pattern_id)
+  }
+
+  @Get(':pattern_id')
+  async findById(@Param('pattern_id') pattern_id: number) {
+    return await this.patternsService.findById(pattern_id)
+  }
+
+  @Get(':pattern_code')
+  async findByCode(@Param('pattern_code') pattern_code: string) {
+    return await this.patternsService.findByCode(pattern_code)
+  }
+
+  @Put(':pattern_id')
+  async update(@Param('pattern_id') pattern_id: number, @Body() updatePatternDto: CreatePatternDto) {
+    return await this.patternsService.update(pattern_id, updatePatternDto)
+  }
+
+  @Delete('')
+  async removeAll() {
+    return await this.patternsService.deleteAllRecords()
+  }
+
   @Get('')
   async getAll() {
     return await this.patternsService.getAll()
   }
 }
+
