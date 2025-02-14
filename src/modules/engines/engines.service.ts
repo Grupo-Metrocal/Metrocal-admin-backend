c6b1508d077e6d5fa0a5511b701b47ff13e8f624
Francisco-Contollo <francisco.garcia@contollo.net>
2025-02-14 15:25:13 -0600

    feat: implement methods for managing alternative paths in EnginesService

--- src/modules/engines/engines.service.ts
+++ src/modules/engines/engines.service.ts
@@ -4,6 +4,7 @@ import { Engine } from './entities/engine.entity'
 import { Repository } from 'typeorm'
 import { handleInternalServerError, handleOK } from 'src/common/handleHttp'
 import * as EngineJSON from './engines.json'
+import * as path from 'path'
 
 @Injectable()
 export class EnginesService {
@@ -52,6 +53,70 @@ export class EnginesService {
       ],
     })
 
-    return engine?.alternative_path || engine?.default_path
+    if (engine?.alternative_path) {
+      return `${path.join(
+        process.env.DESTINATION_CLOUD,
+        'alternative-excel-engine',
+        engine?.alternative_path,
+      )}`
+    }
+
+    return path.join(__dirname, engine?.default_path)
+  }
+
+  async getEnginesByCalibrationMethod(calibration_method: string) {
+    try {
+      const engines = await this.engineRepository.find({
+        where: [
+          {
+            calibration_method,
+          },
+        ],
+      })
+
+      return handleOK(engines)
+    } catch (error) {
+      return handleInternalServerError(error.message)
+    }
+  }
+
+  async setAlternativePath(id: number, path: string) {
+    try {
+      const engine = await this.engineRepository.findOne({
+        where: { id },
+      })
+
+      if (!engine) {
+        return handleInternalServerError('Engine not found')
+      }
+
+      engine.alternative_path = path
+
+      await this.engineRepository.save(engine)
+
+      return handleOK('Path alternativo guardado correctamente')
+    } catch (error) {
+      return handleInternalServerError(error.message)
+    }
+  }
+
+  async deleteAlternativePath(id: number) {
+    try {
+      const engine = await this.engineRepository.findOne({
+        where: { id },
+      })
+
+      if (!engine) {
+        return handleInternalServerError('Engine not found')
+      }
+
+      engine.alternative_path = null
+
+      await this.engineRepository.save(engine)
+
+      return handleOK('Path alternativo eliminado correctamente')
+    } catch (error) {
+      return handleInternalServerError(error.message)
+    }
   }
 }
